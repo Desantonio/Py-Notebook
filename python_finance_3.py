5c0c7e84eeef9db4361b6975cdcf93bcb86dde25
Desantonio <vatsalgayakwad12@aol.com>
2023-08-25 21:38:20 +0530

    Commited

--- python_finance_3.py
+++ python_finance_3.py
@@ -1,9 +1,9 @@
+import sys
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-def stock_function(start, end):
-    stock = input("Enter the stock symbol: ")
-    stock_data = yf.download(stock, start=start, end=end)
+def stock_function(start, end, stock_symbol):
+    stock_data = yf.download(stock_symbol, start=start, end=end)
     
     dates = stock_data.index
     open_prices = stock_data['Open']
@@ -12,12 +12,22 @@ def stock_function(start, end):
     plt.plot(dates, open_prices, label='Open Price')
     plt.xlabel('Date')
     plt.ylabel('Open Price')
-    plt.title(f'Open Prices of {stock} from {start} to {end}')
+    plt.title(f'Open Prices of {stock_symbol} from {start} to {end}')
     plt.legend()
     plt.xticks(rotation=45)
     plt.grid(True)
-    plt.show()
 
-start_date = input("Enter the start date (YYYY-MM-DD): ")
-end_date = input("Enter the end date (YYYY-MM-DD): ")
-stock_function(start_date, end_date)
+    # Save the plot as an image file
+    plt.savefig('/app/plot.png')
+
+    plt.close()  # Close the plot to free up resources
+
+if len(sys.argv) != 4:
+    print("Usage: python python_finance_3.py <start_date> <end_date> <stock_symbol>")
+    sys.exit(1)
+
+start_date = sys.argv[1]
+end_date = sys.argv[2]
+stock_symbol = sys.argv[3]
+
+stock_function(start_date, end_date, stock_symbol)
